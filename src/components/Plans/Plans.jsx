e7deb7cbab09fafe6a58f0944e314c5f528f0f32
younas <younas@localhost.localdomain>
2025-02-04 06:19:04 +0000

    Added some blur styling to the page

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -5,6 +5,8 @@ import tick from '../../assets/tick.png'
 const Plans = () => {
     return (
         <div className="plans-container">
+            <div className="blur plans-blur-1"></div>
+            <div className="blur plans-blur-2"></div>
             <div className="programs-header">
                 <span className='stroke-text'>READY TO START</span><span>YOUR JOURNEY</span><span className='stroke-text'>NOW WITH US</span>
             </div>
